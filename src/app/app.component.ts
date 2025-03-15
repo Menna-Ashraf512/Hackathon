9c97dffa2e06b8d94e9354b440bf0a09f93a367a
Menna-Ashraf512 <mennaalla@gmail.com>
2025-03-15 22:16:43 +0200

    Add Tailwind CSS setup and integrate Font Awesome; initialize Flowbite in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, Inject, PLATFORM_ID, afterNextRender } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
   title = 'Hackathon';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {
+    afterNextRender(() => {
+      if (isPlatformBrowser(this.platformId)) {
+        initFlowbite();
+      }
+    });
+  }
+
+  loadFlowbite(callback: (flowbite: any) => void) {
+    if (isPlatformBrowser(this.platformId)) {
+      import('flowbite').then(flowbite => {
+        callback(flowbite);
+      });
+    }
+  }
 }
